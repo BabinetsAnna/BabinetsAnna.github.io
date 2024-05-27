7fed6809b159e1b3964135a180047feb98cb6d9f
anna babinets <113774821+BabinetsAnna@users.noreply.github.com>
2024-05-27 15:43:05 +0300

    Update Instruments.ts

--- src/instrPages/Instruments.ts
+++ src/instrPages/Instruments.ts
@@ -10,37 +10,37 @@ const instruments: Instrument[] = [
   },
   {
     name: 'Казу',
-    photoUrl: './assets/instruments/kazoo/kazu.jpg',
+    photoUrl: 'https://img.joomcdn.net/e1672d2f7e860c335cf13546c323a51c2aa4a329_1024_1024.jpeg',
     pageName: 'Kazoo'
   },
   {
     name: 'Окарина',
-    photoUrl: './assets/instruments/ocarina/ocarina.webp',
+    photoUrl: 'https://i.etsystatic.com/22951099/r/il/17968d/3426997206/il_570xN.3426997206_oxkr.jpg',
     pageName: 'Ocarina'
   },
   {
     name: 'Терменвокс',
-    photoUrl: './assets/instruments/Theremin/Theremin.jpg',
+    photoUrl: 'https://dt7v1i9vyp3mf.cloudfront.net/styles/news_large/s3/imagelibrary/1/1999-06-theremins-1-H5olsSVXqt_zreY3HxEAWi6VZwsqNJEW.jpg',
     pageName: 'Theremin'
   },
   {
     name: 'Отамaтон',
-    photoUrl: '/src/assets/instruments/otamatone/otamotone.webp',
+    photoUrl: 'https://i5.walmartimages.com/asr/c32a9a5c-2916-4bd7-9414-dd24c3dbf13a.78a496fc971bee9c2080fab090f74672.jpeg?odnHeight=768&odnWidth=768&odnBg=FFFFFF',
     pageName: 'Otamatone'
   },
   {
     name: 'Гітара Пікассо',
-    photoUrl: '/src/assets/instruments/pikasso_guitar/Pikasso-Guitar.jpg',
+    photoUrl: 'https://inartematt.com/wp-content/uploads/2020/09/pika.jpg',
     pageName: 'PikassoGuitar'
   },
   {
     name: 'Дримба',
-    photoUrl: '/src/assets/instruments/jews_harp/drimba.jpeg',
+    photoUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSI5SGMOgfJa_s5FSNTgg3WKihSanQXEchXh61CUeeZ9A&s',
     pageName: 'JewHarp'
   },
   {
     name: 'Гідравлофон',
-    photoUrl: '/src/assets/instruments/Hydraulophone/hydra.jpg',
+    photoUrl: 'https://media.redbullmusicacademy.com/assets/oscfuntain_dusting.194d43d3.jpg',
     pageName: 'Hydraulophone'
   },
 ];
